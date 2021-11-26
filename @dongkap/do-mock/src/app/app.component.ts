4545f8a9766d977a8d929dcef13c2de2f4da9ff0
Ridla Fadilah <ridla.fadilah@gmail.com>
2021-11-27 00:55:35 +0700

    component select

--- @dongkap/do-mock/src/app/app.component.ts
+++ @dongkap/do-mock/src/app/app.component.ts
@@ -3,21 +3,30 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
- import { Component, OnInit } from '@angular/core';
- import { AnalyticsService, SeoService } from '@dongkap/do-core';
+import { Component, Inject, LOCALE_ID, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AnalyticsService, SeoService } from '@dongkap/do-core';
+
+@Component({
+  selector: 'ngx-app',
+  template: '<router-outlet></router-outlet>',
+})
+export class AppComponent implements OnInit {
  
- @Component({
-   selector: 'ngx-app',
-   template: '<router-outlet></router-outlet>',
- })
- export class AppComponent implements OnInit {
+  constructor(
+    private analytics: AnalyticsService,
+    private seoService: SeoService,
+    private translate: TranslateService,
+    @Inject(LOCALE_ID) public locale: string) {
+  }
  
-   constructor(private analytics: AnalyticsService, private seoService: SeoService) {
-   }
- 
-   ngOnInit(): void {
-     this.analytics.trackPageViews();
-     this.seoService.trackCanonicalChanges();
-   }
- }
+  ngOnInit(): void {
+    const localeCode: string = this.locale;
+    this.translate.addLangs(['en-US', 'id-ID']);
+    this.translate.setDefaultLang(localeCode);
+    this.translate.use(localeCode);
+    this.analytics.trackPageViews();
+    this.seoService.trackCanonicalChanges();
+  }
+}
  
